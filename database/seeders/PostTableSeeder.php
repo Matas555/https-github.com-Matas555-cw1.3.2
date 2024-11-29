8b5469913706e514a3744c291cdea71eece16810
2262561 <valikonis.matas557@gmail.com>
2024-11-29 02:06:12 +0100

    Initial commit from PHPSandbox

---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $p = new Post;
+        $p->name = 'Underrated Artist';
+        $p->save();
+
+        Post::factory()->count(10)->create();
+    }
+}
